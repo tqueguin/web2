9a566c92b56e24d09bb01217e8d25c6ff3353365
tanguy.queguineur <tanguy.queguineur@student.vinci.be>
2022-09-23 16:35:16 +0200

    myMovies : module 2 : dynamic homepage

---
+++ exercises/module2/2.2/myMovies/src/index.js
@@ -0,0 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './stylesheets/main.css';
+
+
+import sw from './img/starwars.png';
+import jp from './img/jurassicpark.png'
+
+renderImageInMain(sw);
+renderImageInMain(jp);
+addTextInMain("Welcome to myMovies")
+
+function renderImageInMain(imageName) {
+  const image = new Image(); // or document.createElement('img');
+  image.src = imageName;
+  image.height = 150;
+  const main = document.querySelector('main');
+  main.appendChild(image);
+}
+
+function addTextInMain(text){
+    document.querySelector('header').innerText = text;
+}
