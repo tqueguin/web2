bb9ea078e664a9a1d061ae61d3a2e20155c1730b
tqueguin <tanguyqueguineur@gmail.com>
2022-12-30 21:56:11 +0100

    improve module 2 exercise

--- exercises/module2/2.3/js-basic-boilerplate/src/index.js
+++ exercises/module2/2.3/js-basic-boilerplate/src/index.js
@@ -1,33 +1,49 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
 
-const button = document.querySelector("button");
+const button = document.querySelector("form");
 
 button.addEventListener('submit', (e) => {
     e.preventDefault();
-    document.querySelector("main").style.backgroundColor="red";
+    const lines = document.getElementById("lines").value;
+    const columns = document.getElementById("columns").value;
+    const string = document.getElementById("string").value;
+    const array = createArray(lines,columns,string);
+    const arrayAsHtml = createHtmlTableAsString(array);
+    document.querySelector("#tableDisplay").innerHTML = '';
+    document.querySelector("#tableDisplay").appendChild(arrayAsHtml);
 })
 
 
-/*
-function createArray(lines, columns, string) {
+
+function createArray(lines, columns, string){
     const array = [];
     for (let i = 0; i < lines; i += 1) {
         array.push([]);
         for(let j = 0; j < columns; j += 1){
             array[i].push(`${string}[${i}][${j}]`);
         }
-
     }
     return array;
 }
 
-function createHtmlTableAsString(){
-
+function createHtmlTableAsString(array){
+    const table = document.createElement("table");
+    table.className = "table table-bordered";
+    const tbody = document.createElement("tbody");
+    array.forEach(element => {
+        const trElement = document.createElement("tr");
+        element.forEach(subElement => {
+            const thElement = document.createElement("th");
+            thElement.innerText = subElement;
+            trElement.appendChild(thElement);
+        });
+        tbody.appendChild(trElement);
+    });
+    table.appendChild(tbody);
+    return table;
 }
 
-const tableDisplay = document.querySelector(".tableDisplay");
 
 
 
-*/
