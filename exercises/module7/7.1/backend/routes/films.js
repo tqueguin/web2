d1f01edecd3ba4edd4bf69d557393cc71a6f8b07
tqueguin <tanguyqueguineur@gmail.com>
2022-12-31 23:28:47 +0100

    initial commit exercice 7.1

---
+++ exercises/module7/7.1/backend/routes/films.js
@@ -0,0 +1,138 @@
+// const { json } = require('express');
+const path = require('node:path');
+const express = require('express');
+const { serialize, parse } = require('../utils/json');
+
+const router = express.Router();
+
+const jsonDbPath = path.join(__dirname, '/../data/films.json');
+
+const FILMS = [
+    {
+        id: 1,
+        title: 'Titanic',
+        duration: 150,
+        budget: 200000000,
+        link: 'https://www.imdb.com/title/tt0120338/',
+    },
+];
+
+// READ ALL (FILTERED)
+router.get('/', (req, res) => {
+    console.log('GET /films');
+    const minimumDuration =
+        req?.query['minimum-duration']?.length > 0
+            ? req.query['minimum-duration']
+            : undefined;
+    let listFilms;
+
+    const films = parse(jsonDbPath, FILMS);
+
+    if (minimumDuration) {
+        listFilms = [...films].filter(film => film.duration >= minimumDuration)
+    }
+    else {
+        listFilms = films;
+    }
+    res.json(listFilms);
+});
+
+
+// READ ONE
+router.get('/:id', (req, res) => {
+    console.log(`GET /films/${req.params.id}`);
+    const films = parse(jsonDbPath, FILMS);
+    const idAsANumber = parseInt(req.params.id,10);
+    const indexOfFilmFound = films.findIndex((film) => film.id === idAsANumber);
+    if (indexOfFilmFound < 0) return res.sendStatus(404);
+    return res.json(films[indexOfFilmFound]);
+});
+
+
+// CREATE ONE
+router.post('/', (req, res) => {
+    const title = req?.body?.title?.length !== 0 ? req.body.title : undefined;
+    const duration = req?.body?.duration?.length !== 0 ? req.body.duration : undefined;
+    const budget = req?.body?.budget?.length !== 0 ? req.body.budget : undefined;
+    const link = req?.body?.link?.length !== 0 ? req.body.link : undefined;
+
+    console.log('POST /films');
+
+    if (!title || !duration || !budget || !link) return res.sendStatus(400); // error code '400 Bad request'
+    if (typeof (duration) !== "number" || typeof (budget) !== "number" || duration <= 0 || budget <= 0) return res.sendStatus(400);
+
+    const films = parse(jsonDbPath, FILMS);
+    const lastItemIndex = films?.length !== 0 ? films.length - 1 : undefined;
+    const lastId = lastItemIndex !== undefined ? films[lastItemIndex]?.id : 0;
+    const nextId = lastId + 1;
+
+    const newFilm = {
+        id: nextId,
+        title,
+        duration,
+        budget,
+        link
+    };
+
+    films.push(newFilm);
+
+    serialize(jsonDbPath, films)
+
+    return res.json(newFilm);
+});
+
+// DELETE ONE
+router.delete('/:id', (req, res) => {
+    console.log(`DELETEee /films/${req.params.id}`);
+    
+    const films = parse(jsonDbPath, FILMS);
+
+    const idAsANumber = parseInt(req.params.id, 10);
+    const foundIndex = films.findIndex(film => film.id === idAsANumber);
+    
+    if (foundIndex < 0) return res.sendStatus(404);
+
+    const filmsRemovedFromFilms = films.splice(foundIndex, 1);
+    const filmRemoved = filmsRemovedFromFilms[0];
+
+    serialize(jsonDbPath, films);
+
+    return res.json(filmRemoved);
+});
+
+
+// UPDATE ONE
+
+router.patch('/:id', (req, res) => {
+    console.log(`PATCH /films/${req.params.id}`);
+
+
+    const title = req?.body?.title;
+    const duration = req?.body?.duration;
+    const budget = req?.body?.budget;
+    const link = req?.body?.link;
+
+    console.log('POST /films');
+
+    if ((!title && !duration && !budget && !link) || title?.length === 0 || duration?.length === 0 || budget?.length === 0 || link?.length === 0 ) return res.sendStatus(400);
+    if (typeof (duration) !== "number" || typeof (budget) !== "number" || duration <= 0 || budget <= 0) return res.sendStatus(400);
+    
+    const films = parse(jsonDbPath, FILMS);
+
+    const idAsANumber = parseInt(req.params.id, 10);
+
+    const foundIndex = films.findIndex(film => film.id === idAsANumber);
+
+    if (foundIndex < 0) return res.sendStatus(404);
+
+    const updatedFilm = { ...films[foundIndex], ...req.body };
+
+    films[foundIndex] = updatedFilm;
+
+    serialize(jsonDbPath, films);
+
+    return res.json(updatedFilm);
+});
+
+
+module.exports = router;
---
+++ exercises/module7/7.1/frontend/src/Components/Pages/ViewPage.js
@@ -0,0 +1,152 @@
+import { clearPage } from '../../utils/render';
+
+
+const ViewPage = async () => {
+  try {
+    clearPage();
+
+    const films = await getAllFilms();
+    document.getElementById('mainContent').appendChild(createMovieTable(films));
+  } catch (err) {
+    console.error('ViewPage::error: ', err);
+  }
+};
+
+async function getAllFilms() {
+  try {
+    const response = await fetch('/api/films');
+
+    if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
+
+    const films = await response.json();
+
+    return films;
+  } catch (err) {
+    console.error('getAllFilms::error: ', err);
+    throw err;
+  }
+}
+
+function createMovieTable(films) {
+  const table = document.createElement("table");
+  table.className = "table table-bordered";
+  const thead = document.createElement("thead");
+  const tableTitles = document.createElement("tr");
+  const title1 = document.createElement("th");
+  title1.innerText = "Title";
+  tableTitles.appendChild(title1);
+  const title2 = document.createElement("th");
+  title2.innerText = "Link";
+  tableTitles.appendChild(title2);
+  const title3 = document.createElement("th");
+  title3.innerText = "Duration (min)";
+  tableTitles.appendChild(title3);
+  const title4 = document.createElement("th");
+  title4.innerText = "Budget (million)";
+  tableTitles.appendChild(title4);
+  const title5 = document.createElement("th");
+  title5.innerText = "Operations";
+  tableTitles.appendChild(title5);
+  thead.appendChild(tableTitles);
+  table.appendChild(thead);
+
+
+  const tbody = document.createElement("tbody");
+  films.forEach(film => {
+    const line = document.createElement("tr");
+    const filmTitle = document.createElement("th");
+    filmTitle.innerText = film.title;
+    filmTitle.contentEditable = true;
+    line.appendChild(filmTitle);
+    const filmLink = document.createElement("th");
+    filmLink.innerHTML = `<a href=${film.link}>${film.link}</a>`;
+    filmLink.contentEditable = true;
+    line.appendChild(filmLink);
+    const filmDuration = document.createElement("th");
+    filmDuration.innerText = film.duration;
+    filmDuration.contentEditable = true;
+    line.appendChild(filmDuration);
+    const filmBudget = document.createElement("th");
+    filmBudget.innerText = film.budget;
+    filmBudget.contentEditable = true;
+    line.appendChild(filmBudget);
+    const operations = document.createElement("th");
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.id = film.id;
+    deleteButton.addEventListener("click", deleteOneMovie);
+    const saveButton = document.createElement("button");
+    saveButton.innerText = "Save";
+    saveButton.id = film.id;
+    saveButton.addEventListener("click", saveOneMovie);
+    operations.appendChild(deleteButton);
+    operations.appendChild(saveButton);
+    line.appendChild(operations);
+
+    tbody.appendChild(line);
+  });
+  table.appendChild(tbody);
+  return table;
+}
+
+async function deleteOneMovie(e) {
+  const movieId = e.target.id;
+
+  try {
+    const options = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const response = await fetch(`/api/films/${movieId}`, options);
+
+    if (!response.ok) {
+      throw new Error(`deleteOneMovie:: fetch error : ${response.status} : ${response.statusText}`);
+    }
+  } catch (err) {
+    console.error('deleteOneMovie::error: ', err);
+    throw err;
+  }
+
+}
+
+async function saveOneMovie(e) {
+  const movieId = e.target.id;
+  
+  const td = e.target.parentElement.parentElement;
+  const title = td.children[0].innerText;
+  const link = td.children[1].innerText;
+  const duration = td.children[2].innerText;
+  const budget = td.children[3].innerText;
+
+
+  try {
+    const options = {
+      method: 'PATCH',
+      body: JSON.stringify({
+        title,
+        link,
+        budget,
+        duration
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const response = await fetch(`/api/films/${movieId}`, options);
+
+    if (!response.ok) {
+      throw new Error(`deleteOneMovie:: fetch error : ${response.status} : ${response.statusText}`);
+    }
+  } catch (err) {
+    console.error('deleteOneMovie::error: ', err);
+    throw err;
+  }
+
+}
+
+
+export default ViewPage;
